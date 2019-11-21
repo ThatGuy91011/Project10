84768e09a6deb2ff5ca97f927c09c23b7b77277f
KIRK WALLS <KIRWALLS@uat.edu>
2019-11-21 13:17:50 -0700

    Second Commit

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 
 using namespace std;
+void bank(int& balance);
+void bankPointer(int* balance);
 
 int main()
 {
-	cout << "\t\tPointer test" << endl;
+	/**cout << "\t\tPointer test" << endl;
 
 	int age = 21;
 	int* page = &age;
 
 	cout << "The value of 'age' is " << *page << endl;
+	**/
+
+	int balance = 300;
+
+	bank(balance);
+
+	cout << "Balance = " << balance;
+
+	bankPointer(&balance);
+	
+	cout << "Balance = " << balance;
+}
+
+void bank(int& balance)
+{
+	balance = 500;
+}
+
+void bankPointer(int* balance)
+{
+	*balance = 900;
 }
